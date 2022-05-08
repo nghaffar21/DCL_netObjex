57e39ae57fe24d49055c8524007411c6a93437b6
abdeladim-naitbrahim <bnight@gmail.com>
2022-05-08 01:37:32 +0000

    html

--- src/gameobjects/Dianonds.ts
+++ src/gameobjects/Dianonds.ts
@@ -1,9 +1,10 @@
+import { links } from "src/nftslinks";
 import { Diamond } from "src/prefabs/Diamond";
 
 export function Diamonds(building) 
 {
-    new Diamond(building,new Vector3(-12.5, 13.5, 5.3),"https://www.google.com",1);
-    new Diamond(building,new Vector3(-12.5, 13.5, 2.5),"https://www.google.com",2);
-    new Diamond(building,new Vector3(-10, 13.5, 5.3),"https://www.google.com",3);
-    new Diamond(building,new Vector3(-10, 13.5, 2.5),"https://www.google.com",4);
+    new Diamond(building,new Vector3(-12.5, 13.5, 5.3),links.floor0[4][0].link,1);
+    new Diamond(building,new Vector3(-12.5, 13.5, 2.5),links.floor0[5][0].link,2);
+    new Diamond(building,new Vector3(-10, 13.5, 5.3),links.floor0[6][0].link,3);
+    new Diamond(building,new Vector3(-10, 13.5, 2.5),links.floor0[7][0].link,4);
 }
